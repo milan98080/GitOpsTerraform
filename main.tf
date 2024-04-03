ea7ca272f4e21c4b85be7a1a4b9448d6ba1f9370
milan98080 <mee.lawn222@gmail.com>
2024-04-04 05:36:27 +0545

    fix: ACL added

--- main.tf
+++ main.tf
@@ -26,6 +26,39 @@ resource "aws_s3_bucket_versioning" "frontend_dist_versioning" {
   }
 }
 
+resource "aws_s3_bucket_ownership_controls" "owner" {
+  bucket = aws_s3_bucket.frontend_dist.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [aws_s3_bucket_ownership_controls.owner]
+
+  bucket = aws_s3_bucket.frontend_dist.id
+  access_control_policy {
+    grant {
+      grantee {
+        id   = data.aws_canonical_user_id.current.id
+        type = "CanonicalUser"
+      }
+      permission = "WRITE"
+    }
+
+    grant {
+      grantee {
+        type = "Group"
+        uri  = "http://acs.amazonaws.com/groups/s3/LogDelivery"
+      }
+      permission = "WRITE"
+    }
+
+    owner {
+      id = data.aws_canonical_user_id.current.id
+    }
+  }
+}
 resource "aws_s3_bucket_server_side_encryption_configuration" "frontend_dist_encryption" {
   bucket = aws_s3_bucket.frontend_dist.id
   rule {
