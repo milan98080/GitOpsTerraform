aa3983c9f0ff9d8f0763ebcb8ae2891f72de9079
Milan Pokhrel <54058328+milan98080@users.noreply.github.com>
2024-04-04 00:15:12 +0545

    minor: removed type

--- variables.tf
+++ variables.tf
@@ -1,11 +1,9 @@
 variable "region" {
   description = "The region in which resources will be created"
-  type        = string
   default     = "us-east-1"
 }
 
 variable "s3_dist_bucket" {
   description = "The name of the S3 bucket to store the website content"
-  type        = string
   default     = "milan-s3-frontend-bucket"
 }
